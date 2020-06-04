f3d0e031ec2b4444c9f88ac94bfc4665a40231ef
Nadir Hamid <matrix.nad@gmail.com>
2020-06-04 07:45:25 +0000

    update docker

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,3 +1,3 @@
-gulp scripts
-gulp compress-js
+PATH=$PATH:/usr/share/node-v11.10.1-linux-x64/bin/ gulp scripts
+PATH=$PATH:/usr/share/node-v11.10.1-linux-x64/bin/ gulp compress-js
 yes | cp index.prod.html index.html
